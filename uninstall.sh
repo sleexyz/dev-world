1352a7486c3b1d60026090a6059919ac930e8de1
Sean Lee <freshdried@gmail.com>
2023-04-19 17:28:00 -0400

    Do not uninstall if not installed.

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+if [[  -z $(launchctl list | grep dev-world) ]]; then
+    echo "dev-world is not installed"
+    exit 1
+fi
 launchctl stop dev-world
 launchctl unload -w ~/Library/LaunchAgents/dev-world.plist
 rm ~/Library/LaunchAgents/dev-world.plist
-pkill -f code-server
