d8d48868637925ee58938adcedc393a9a6abfc56
Sean Lee <freshdried@gmail.com>
2023-04-23 15:15:00 -0400

    Load extension editor in iframe.

--- extension/src/panel_script.ts
+++ extension/src/panel_script.ts
@@ -1,1 +1,9 @@
-window.location.href = "http://localhost:12345/";
+const frame = Object.assign(document.createElement("iframe"), {
+    src: "http://localhost:12345/",
+    style: "position: absolute; top: 0; left: 0; border: none;",
+    width: "100%",
+    height: "100%",
+    allow: "clipboard-read; clipboard-write",
+});
+
+document.body.appendChild(frame);
