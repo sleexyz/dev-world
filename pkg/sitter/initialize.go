4880eaa078645c8e086e0621ffa1c1993180d4ac
Sean Lee <freshdried@gmail.com>
2023-04-26 18:53:52 -0400

    Restore state from state file.

--- pkg/sitter/initialize.go
+++ pkg/sitter/initialize.go
@@ -2,12 +2,8 @@ package sitter
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"strconv"
-	"strings"
 
 	"github.com/sleexyz/dev-world/pkg/workspace"
 )
@@ -19,16 +15,11 @@ func LoadSitter() *Sitter {
 
 	// Attempt reconnection to existing workspaces, and then re-save the sitter state
 	for _, ws := range sitterState.Workspaces {
-		w, err := sitter.reconnectToWorkspace(context.Background(), ws)
-		// If we fail to reconnect to a workspace, remove the stale socket
+		w, err := sitter.restoreWorkspace(context.Background(), ws)
 		if err != nil {
-			codeServerSocketPath := workspace.GetCodeServerSocketPath(ws.Path)
-			err := os.Remove(codeServerSocketPath)
-			if err != nil {
-				log.Fatalf("Failed to remove existing socket: %v", err)
-			}
-			log.Printf("Removed stale socket at %s\n", ws.Path)
+			continue
 		}
+		log.Printf("Reconnected to workspace at %s\n", ws.Path)
 		sitter.addWorkspace(w)
 	}
 	sitter.SaveSitter()
@@ -74,50 +65,29 @@ func (s *Sitter) SaveSitter() {
 	SaveSitterState(&SitterState)
 }
 
-func (s *Sitter) reconnectToWorkspace(ctx context.Context, ws *WorkspaceState) (*workspace.Workspace, error) {
-	codeServerSocketPath := workspace.GetCodeServerSocketPath(ws.Path)
-
-	_, err := os.Stat(codeServerSocketPath)
+func (s *Sitter) restoreWorkspace(ctx context.Context, ws *WorkspaceState) (*workspace.Workspace, error) {
+	// Check if the socket exists
+	_, err := os.Stat(ws.Socket)
 	if err != nil {
 		return nil, err
 	}
 
-	process, err := getCodeServerProcess(ctx, codeServerSocketPath)
+	// Check if the process exists
+	process, err := os.FindProcess(ws.Pid)
 	if err != nil {
 		return nil, err
 	}
 
-	// If the socket already exists, try to reconnect to it
 	w := &workspace.Workspace{
 		Path:    ws.Path,
-		Socket:  codeServerSocketPath,
+		Socket:  ws.Socket,
 		Process: process,
 	}
-	err = w.WaitForSocket(ctx)
-	if err != nil {
-		return nil, err
-	}
-	log.Printf("Reconnecting to existing socket for %s\n", ws.Path)
-	return w, nil
-}
 
-func getCodeServerProcess(ctx context.Context, socketPath string) (*os.Process, error) {
-	cmd := exec.CommandContext(ctx, "pgrep", "-f", socketPath)
-	out, err := cmd.Output()
-	if err != nil {
+	// Check that socket is still alive
+	if err := w.WaitForSocket(ctx); err != nil {
 		return nil, err
 	}
-	outs := strings.Split(string(out), "\n")
-	for _, out := range outs {
-		pid, err := strconv.Atoi(string(out))
-		if err != nil {
-			continue
-		}
-		process, err := os.FindProcess(pid)
-		if err != nil {
-			continue
-		}
-		return process, nil
-	}
-	return nil, fmt.Errorf("no matching process found")
+
+	return w, nil
 }
