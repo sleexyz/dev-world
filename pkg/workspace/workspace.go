ab252e3f78fc7edc5532a8cecb543730ec181bec
Sean Lee <freshdried@gmail.com>
2023-04-26 19:32:40 -0400

    Save the vscode-ipc socket on creation.

--- pkg/workspace/workspace.go
+++ pkg/workspace/workspace.go
@@ -19,9 +19,10 @@ import (
 )
 
 type Workspace struct {
-	Path    string
-	Socket  string
-	Process *os.Process
+	Path         string
+	Socket       string
+	VscodeSocket string
+	Process      *os.Process
 }
 
 func (workspace *Workspace) ReverseProxy(w http.ResponseWriter, r *http.Request) error {
@@ -59,7 +60,7 @@ func (workspace *Workspace) ReverseProxy(w http.ResponseWriter, r *http.Request)
 	}
 }
 
-func (workspace *Workspace) WaitForSocket(ctx context.Context) error {
+func WaitForSocket(ctx context.Context, socket string) error {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	backoff := time.Millisecond * 100
@@ -68,7 +69,7 @@ func (workspace *Workspace) WaitForSocket(ctx context.Context) error {
 		case <-ctx.Done():
 			return ctx.Err()
 		default:
-			conn, err := net.Dial("unix", workspace.Socket)
+			conn, err := net.Dial("unix", socket)
 			if err == nil {
 				conn.Close()
 				return nil
@@ -154,15 +155,21 @@ func CreateWorkspace(ctx context.Context, path string) *Workspace {
 		log.Fatalf("Failed to start child process: %v", err)
 	}
 
-	workspace := &Workspace{
-		Path:    path,
-		Socket:  codeServerSocketPath,
-		Process: cmd.Process,
-	}
-
-	err = workspace.WaitForSocket(ctx)
+	err = WaitForSocket(ctx, codeServerSocketPath)
 	if err != nil {
 		log.Printf("Failed health check for child process: %v", err)
+	}
+	// HACK: We determine the vscode-ipc socket path by reading $TMPDIR/vscode-ipc
+	// directly after creating the new code-server process. This may be prone to
+	// race conditions.
+	vscodeSocketPath := GetVscodeSocketPath()
+	log.Printf("vscode-ipc socket path: %s\n", vscodeSocketPath)
+
+	workspace := &Workspace{
+		Path:         path,
+		Socket:       codeServerSocketPath,
+		VscodeSocket: vscodeSocketPath,
+		Process:      cmd.Process,
 	}
 
 	return workspace
@@ -171,6 +178,15 @@ func CreateWorkspace(ctx context.Context, path string) *Workspace {
 func GetCodeServerSocketPath(folder string) string {
 	key := CreateKeyFromPath(folder)
 	return fmt.Sprintf("%scode-server-%s.sock", os.TempDir(), key)
+}
+
+// Reads the vscode-ipc socket path from $TMPDIR/vscode-ipc
+func GetVscodeSocketPath() string {
+	b, err := os.ReadFile(os.TempDir() + "vscode-ipc")
+	if err != nil {
+		log.Fatalf("Failed to read vscode-ipc socket path: %v", err)
+	}
+	return string(b)
 }
 
 func GetFolderFromSocketPath(socketPath string) (string, error) {
