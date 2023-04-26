de92b41bf444861dc9acafe1a9e8ce94235c8bc5
Sean Lee <freshdried@gmail.com>
2023-04-25 20:19:32 -0400

    Dynamically change title based off hostname.

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,17 +1,37 @@
 import App from "./App";
 import { EditorContainer } from "./EditorContainer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/:alias",
-    element : <EditorContainer/>,
+    element: <Container />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: ":alias",
+        element: <EditorContainer />,
+      },
+    ],
   },
 ]);
+
+function Container() {
+  const location = useLocation();
+  useEffect(() => {
+    document.title = `${window.location.host}${window.location.pathname}`;
+  }, [location]);
+  return <Outlet/>;
+}
 
 export function Router() {
   return <RouterProvider router={router} />;
