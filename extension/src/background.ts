3e2d745a7c80603e7c8c9a7fc9c0f0212191ae0a
Sean Lee <freshdried@gmail.com>
2023-04-27 14:20:42 -0400

    Fix extension lifecycle behavior.

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,75 +1,93 @@
 const PACMAN_EXTENSION_ID = "jbclhgpgaijegjnfhbpidgaooihpcphd";
 
-chrome.runtime.onInstalled.addListener(async () => {
-  //Install PAC script on install by messaging the pacman extension
-  await Promise.all([
-    chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
-      setProxyRequest: {
-        protocol: "http",
-        host: "dev",
-        type: "PROXY",
-        destination: "localhost:12345",
-      },
-    }),
-    chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
-      setProxyRequest: {
-        protocol: "https",
-        host: "dev",
-        type: "HTTPS",
-        destination: "localhost:12345",
-      },
-    }),
-    chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
-      setProxyRequest: {
-        protocol: "http",
-        host: "d",
-        type: "PROXY",
-        destination: "localhost:12345",
-      },
-    }),
-    chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
-      setProxyRequest: {
-        protocol: "https",
-        host: "d",
-        type: "HTTPS",
-        destination: "localhost:12345",
-      },
-    }),
-  ]);
-});
+class Extension {
+  async registerExtension() {
+    await Promise.all([
+      chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
+        setProxyRequest: {
+          protocol: "http",
+          host: "dev",
+          type: "PROXY",
+          destination: "localhost:12345",
+        },
+      }),
+      chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
+        setProxyRequest: {
+          protocol: "https",
+          host: "dev",
+          type: "HTTPS",
+          destination: "localhost:12345",
+        },
+      }),
+      chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
+        setProxyRequest: {
+          protocol: "http",
+          host: "d",
+          type: "PROXY",
+          destination: "localhost:12345",
+        },
+      }),
+      chrome.runtime.sendMessage(PACMAN_EXTENSION_ID, {
+        setProxyRequest: {
+          protocol: "https",
+          host: "d",
+          type: "HTTPS",
+          destination: "localhost:12345",
+        },
+      }),
+    ]);
+  }
 
-loop({ maxRunsPerSec: 1 }, async () => {
-  // Listen to SSE events from /api/listen-open-file
-  const eventSource = new EventSource(
-    "https://localhost:12345/api/listen-open-file"
-  );
+  listen() {
+    runForever({ maxRunsPerSec: 3 }, async () => {
+      const eventSource = new EventSource(
+        "https://localhost:12345/api/listen-open-file"
+      );
+      eventSource.addEventListener("open", onOpen) 
+      eventSource.addEventListener("message", onMessage);
 
-  // Listen to the "open" event
-  eventSource.addEventListener("open", (event: Event) => {
-    console.log("Connection opened", event);
-  });
-  // Listen to the "message" event
-  eventSource.addEventListener("message", (event: MessageEvent) => {
-    console.log("Message received:", event.data);
-    openFile(JSON.parse(event.data));
-  });
+      function onOpen (event: Event)  {
+        console.log("Connection opened", event);
+      }
+      function onMessage (event: MessageEvent) {
+        console.log("Message received:", event.data);
+        openFile(JSON.parse(event.data));
+      }
 
-  const cbs: { resolve: () => void; reject: (reason?: unknown) => void }[] = [];
-  eventSource.addEventListener("error", (event: Event) => {
-    console.log("Error occurred:", event);
-    for (const cb of cbs) {
-      cb.reject();
-    }
-  });
-  await new Promise<void>((resolve, reject) => {
-    cbs.push({ resolve, reject });
-  });
-});
+      // Wait for error
+      const cbs: { resolve: () => void; reject: (reason?: unknown) => void }[] =
+        [];
+      eventSource.addEventListener("error", onError);
+      function onError(event: Event) {
+        console.log("Error occurred:", event);
+        for (const cb of cbs) {
+          cb.reject();
+        }
+      }
+      await new Promise<void>((resolve, reject) => {
+        cbs.push({ resolve, reject });
+      });
+
+      // Cleanup
+      eventSource.removeEventListener("error", onError);
+      eventSource.removeEventListener("open", onOpen);
+      eventSource.removeEventListener("message", onMessage);
+      eventSource.close();
+    });
+  }
+}
+
+(async () => {
+  const extension = new Extension();
+  await extension.registerExtension();
+  console.log("Registered extension");
+})();
 
 function openFile({ file }: { file: string; line: number; column: number }) {
   console.log("Opening file", file);
   openTabForWorkspace(file);
 }
+
 // Iterate through all tabs and focus on the first matching tab
 function openTabForWorkspace(file: string) {
   chrome.tabs.query({}, (tabs) => {
@@ -94,22 +112,18 @@ function openTabForWorkspace(file: string) {
   });
 }
 
-async function loop(
+async function runForever(
   options: { maxRunsPerSec: number },
   fn: () => Promise<void>
 ) {
-  let runs = 0;
-  let lastSampledTime = Date.now();
   for (;;) {
+    const start = Date.now();
     await fn();
-    runs += 1;
-    // Break if we've run 3 * n times within 3 seconds, aka avg of n times per second:
-    if (runs >= 3) {
-      if (Date.now() - lastSampledTime < 3 * 1000 * options.maxRunsPerSec) {
-        break;
-      }
-      lastSampledTime = Date.now();
-      runs = 0;
+    const timeElapsed = Date.now() - start;
+    if (timeElapsed < 1000 / options.maxRunsPerSec) {
+      await new Promise((resolve) =>
+        setTimeout(resolve, 1000 / options.maxRunsPerSec - timeElapsed)
+      );
     }
   }
 }
--- pacman/src/background.ts
+++ pacman/src/background.ts
@@ -5,11 +5,16 @@ interface ProxyEntry {
   destination: string;
   extensionId: string;
 }
+interface SetProxyRequest {
+  protocol: string;
+  host: string;
+  type: string;
+  destination: string;
+}
+
+export type SetProxyResponse = boolean;
 
 class ProxyManager {
-  static instance: ProxyManager = new ProxyManager();
-
-  // alias -> ProxyEntry
   constructor(readonly entries: Record<string, ProxyEntry> = {}) {}
 
   private generatePacScript(): string {
@@ -26,13 +31,9 @@ function FindProxyForURL(url, host) {
 `;
   }
 
-  static makeEntryKey(protocol: string, host: string) {
-    return `${protocol}://${host}`;
-  }
   addEntry(entry: ProxyEntry) {
-    const key = ProxyManager.makeEntryKey(entry.protocol, entry.host)
-    this.entries[key] =  entry;
-    this.applySettings();
+    const key = entry.protocol + "://" + entry.host;
+    this.entries[key] = entry;
   }
 
   applySettings() {
@@ -47,54 +48,72 @@ function FindProxyForURL(url, host) {
         },
       },
       () => {
-        console.log("Set PAC script: ", pacScript)
+        console.log("Set PAC script: ", pacScript);
       }
     );
-  }
-
-  load() {
-    chrome.storage.local.get("entries", (result) => {
-      if (result.entries) {
-        for (const entry of Object.values(result.entries as Record<string, ProxyEntry>)) {
-          this.addEntry(entry);
-        }
-      }
-    });
   }
 
   save() {
     chrome.storage.local.set({ entries: this.entries });
   }
-}
 
-// Install PAC script on install
-chrome.runtime.onInstalled.addListener(() => {
-  ProxyManager.instance.load();
-  ProxyManager.instance.applySettings();
-});
+  static async load(): Promise<ProxyManager> {
+    const proxyManager = await new Promise<ProxyManager>((resolve) => {
+      chrome.storage.local.get("entries", (result) => {
+        const instance = new ProxyManager();
+        if (result.entries) {
+          for (const entry of Object.values(
+            result.entries as Record<string, ProxyEntry>
+          )) {
+            instance.addEntry(entry);
+          }
+        }
+        resolve(instance);
+      });
+    });
+    proxyManager.applySettings();
+    return proxyManager;
+  }
 
-
-interface SetProxyRequest {
-  protocol: string;
-  host: string;
-  type: string;
-  destination: string;
-}
-
-export type SetProxyResponse = boolean;
-
-chrome.runtime.onMessageExternal.addListener(
-  (request, sender, sendResponse) => {
-    if (request.setProxyRequest) {
-      const { protocol, host, type, destination } = request.setProxyRequest as SetProxyRequest;
-      const { id: extensionId } = sender;
-      if (!extensionId) {
-        sendResponse({ setProxyResponse: false });
-        return;
-      }
-      ProxyManager.instance.addEntry({ protocol, host, type, destination, extensionId });
-      ProxyManager.instance.save();
-      sendResponse({ setProxyResponse: true });
+  async handleSetProxyRequest(
+    { protocol, host, type, destination }: SetProxyRequest,
+    extensionId: string
+  ): Promise<SetProxyResponse> {
+    try {
+      this.addEntry({
+        protocol,
+        host,
+        type,
+        destination,
+        extensionId,
+      });
+      this.save();
+      this.applySettings();
+      return true;
+    } catch (e) {
+      return false;
     }
   }
-);
+
+  listen() {
+    chrome.runtime.onMessageExternal.addListener(
+      async (request, sender, sendResponse) => {
+        if (!sender.id) {
+          return;
+        }
+        if (request.setProxyRequest) {
+          const setProxyResponse = await this.handleSetProxyRequest(
+            request.setProxyRequest as SetProxyRequest,
+            sender.id
+          );
+          sendResponse({ setProxyResponse });
+        }
+      }
+    );
+  }
+}
+
+(async () => {
+  const proxyManager = await ProxyManager.load();
+  proxyManager.listen();
+})();
