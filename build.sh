07cea5d19bfb72cf7f262ae8ccce582d2cc59e71
Sean Lee <freshdried@gmail.com>
2023-05-30 23:41:13 -0400

    Propagate exit code for build command.

--- build.sh
+++ build.sh
@@ -3,5 +3,13 @@
 mkdir -p bin
 
 nix develop --command go build -o bin/serve ./pkg/serve
-chmod +x bin/serve
-echo "Built bin/serve"
+CODE=$?
+
+if [ $CODE -eq 0 ]; then
+	chmod +x bin/serve
+	echo "Built bin/serve"
+	exit 0
+fi
+
+echo "Failed to build bin/serve"
+exit 1
